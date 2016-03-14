25d60a783604eabc9a58d57fad009fbefcd30ab0
Mbob023 <armcgrathmedia@gmail.com>
2016-03-14 17:32:39 -0500

    heading home update

--- prime-solo-sql-db.sql
+++ prime-solo-sql-db.sql
@@ -1,1 +1,13 @@
-—
+—- 1. users from Chicago = shawn, anthony, and davey
+
+—- 2. usernames that contain the letter ‘a’ = shawn, larry, dallas, anthony, travis, davey, and ora
+
+—- 3. anthony had a balance of 0 and 0 transactions and now has a balance of $10
+
+—- 4. users that have 9 or more transactions attempted include: shawn, cherise, dallas, travis, davey, and ora
+
+—- 5. users with balances over $100 from highest to lowest are travis, cherise, and shawn
+
+—- 6. the three lowest balances in from lowest to highest are dallas, ora, and anthony
+
+—- 7.
